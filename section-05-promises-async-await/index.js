40b574e444cdd3e6990116bd9a0e3a65ff765c3b
arigatopix <teeruch.k@gmail.com>
2020-09-29 22:56:40 +0700

    43. Building Promises

--- section-05-promises-async-await/index.js
+++ section-05-promises-async-await/index.js
@@ -27,24 +27,71 @@ const superagent = require('superagent');
 /*
  * 42. From Callback Hell to Promises
  */
-fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
-  console.log(`Breed: ${data}`);
+// fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
+//   console.log(`Breed: ${data}`);
 
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`) // Promises
-    .then((res) => {
-      // resolved Promises object
-      console.log(res.body.message);
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`) // Promises
+//     .then((res) => {
+//       // resolved Promises object
+//       console.log(res.body.message);
 
-      // write to file
-      fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, (err) => {
-        if (err) return console.log(err.message);
+//       // write to file
+//       fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, (err) => {
+//         if (err) return console.log(err.message);
 
-        console.log(`Random dog image save to file...`);
-      });
-    })
-    .catch((err) => {
-      // rejected error
-      return console.log(err.message);
+//         console.log(`Random dog image save to file...`);
+//       });
+//     })
+//     .catch((err) => {
+//       // rejected error
+//       return console.log(err.message);
+//     });
+// });
+
+/* ****************
+ * 43. Building Promises
+ */
+
+// create Read Promises object เพื่อ handle .then .catch
+const readFilePromises = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      //rejected
+      if (err) reject('I could not find that file :(');
+
+      // fullfilled
+      resolve(data);
     });
-});
+  });
+};
+
+// create Write Promises
+const writeFilePromises = (file, res) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, res, (err) => {
+      if (err) reject('Could not write file :(');
+
+      resolve('success');
+    });
+  });
+};
+
+// Execute Promises chain
+readFilePromises(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    // resolved Promises object
+    console.log(res.body.message);
+
+    // write to file
+    return writeFilePromises(`${__dirname}/dog-img.txt`, res.body.message);
+  })
+  .then(console.log(`Random dog image save to file...`))
+  .catch((err) => {
+    // rejected error
+    console.log(err);
+  });
