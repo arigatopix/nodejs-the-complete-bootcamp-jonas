68b09c0a509e17e01845d5b501b8386fbebaa15c
arigatopix <teeruch.k@gmail.com>
2020-09-29 22:01:27 +0700

    42. From Callback Hell to Promises

--- section-05-promises-async-await/index.js
+++ section-05-promises-async-await/index.js
@@ -1,15 +1,39 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
-  // fetch data from dog.ceo
-  superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-      if (err) return console.log(err.message);
+/*
+ * 41. The Problem with Callbacks: Callback Hell
+ */
+// fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
+//   // fetch data from dog.ceo
+//   superagent
+//     .get(`https://dog.ceo/api/breed/${data}/images/random`)
+//     .end((err, res) => {
+//       if (err) return console.log(err.message);
 
-      // ได้ obj ที่ส่งมาจาก api
-      // ต้องไล่หาเอาจาก res
+//       // ได้ obj ที่ส่งมาจาก api
+//       // ต้องไล่หาเอาจาก res
+//       console.log(res.body.message);
+
+//       // write to file
+//       fs.writeFile(`${__dirname}/dog-img.txt`, res.body.message, (err) => {
+//         if (err) return console.log(err.message);
+
+//         console.log(`Random dog image save to file...`);
+//       });
+//     });
+// });
+
+/*
+ * 42. From Callback Hell to Promises
+ */
+fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
+  console.log(`Breed: ${data}`);
+
+  superagent
+    .get(`https://dog.ceo/api/breed/${data}/images/random`) // Promises
+    .then((res) => {
+      // resolved Promises object
       console.log(res.body.message);
 
       // write to file
@@ -18,5 +42,9 @@ fs.readFile(`${__dirname}/dog.txt`, 'utf8', (err, data) => {
 
         console.log(`Random dog image save to file...`);
       });
+    })
+    .catch((err) => {
+      // rejected error
+      return console.log(err.message);
     });
 });
