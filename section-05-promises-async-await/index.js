18b568b10c30ead118a9806bb2334debf404f322
arigatopix <teeruch.k@gmail.com>
2020-09-30 10:42:43 +0700

    46. Waiting for Multiple Promises Simultaneously

--- section-05-promises-async-await/index.js
+++ section-05-promises-async-await/index.js
@@ -150,6 +150,86 @@ const superagent = require('superagent');
  * 45. Returning Values from Async Functions
  *****/
 
+// // create Read Promises object เพื่อ handle .then .catch
+// const readFilePromises = (file) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(file, (err, data) => {
+//       //rejected
+//       if (err) reject('I could not find that file :(');
+
+//       // fullfilled
+//       resolve(data);
+//     });
+//   });
+// };
+
+// // create Write Promises
+// const writeFilePromises = (file, res) => {
+//   return new Promise((resolve, reject) => {
+//     fs.writeFile(file, res, (err) => {
+//       if (err) reject('Could not write file :(');
+
+//       resolve('success');
+//     });
+//   });
+// };
+
+// const getRandomPicture = async () => {
+//   try {
+//     // readFile from txt เป็น Promises
+//     const data = await readFilePromises(`${__dirname}/dog.txt`);
+//     console.log(`Breed: ${data}`);
+
+//     // fetch data from API
+//     const res = await await superagent.get(
+//       `https://dog.ceo/api/breed/${data}/images/random`
+//     );
+//     console.log(res.body.message);
+
+//     // writeFile แบบ Promises
+//     await writeFilePromises(`${__dirname}/dog-img.txt`, res.body.message);
+//   } catch (err) {
+//     // throw err -> rejected Promise
+//     throw err;
+//   }
+
+//   // return result from Promise
+//   return '2: READY !!!';
+// };
+
+// // รับค่า result จาก async...await โดยใช้ async...await
+// console.log('1: Will get dog pics!');
+
+// (async () => {
+//   try {
+//     const result = await getRandomPicture();
+//     console.log(result);
+//     console.log('3:Done Getting dog pics!');
+
+//     return result;
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
+// รับค่า result จาก async...await โดยใช้ .then(), .catch() method
+/* 
+console.log('1: Will get dog pics!');
+getRandomPicture()
+  .then((result) => {
+    console.log(result);
+    console.log('3:Done Getting dog pics!');
+  })
+  .catch((err) => {
+    // รับ result ที่เป็น error ใน async...await
+    console.log(err);
+  });
+ */
+
+/* **
+ * 46. Waiting for Multiple Promises Simultaneously
+ *****/
+
 // create Read Promises object เพื่อ handle .then .catch
 const readFilePromises = (file) => {
   return new Promise((resolve, reject) => {
@@ -181,13 +261,29 @@ const getRandomPicture = async () => {
     console.log(`Breed: ${data}`);
 
     // fetch data from API
-    const res = await await superagent.get(
+    const res1Promise = superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
+
+    const res2Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    const res3Promise = superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+
+    // ใช้รับค่า resolve ของ Promises โดย return array
+    const all = await Promise.all([res1Promise, res2Promise, res3Promise]);
+
+    // loop through array เอาเฉพาะ url
+    const imgs = all.map((el) => el.body.message);
+
+    console.log(imgs);
 
     // writeFile แบบ Promises
-    await writeFilePromises(`${__dirname}/dog-img.txt`, res.body.message);
+    // .join('\n') คือ join text โดยมี \n (new line) ระหว่างกลางของ text
+    await writeFilePromises(`${__dirname}/dog-img.txt`, imgs.join('\n'));
   } catch (err) {
     // throw err -> rejected Promise
     throw err;
@@ -211,17 +307,3 @@ console.log('1: Will get dog pics!');
     console.log(err);
   }
 })();
-
-// รับค่า result จาก async...await โดยใช้ .then(), .catch() method
-/* 
-console.log('1: Will get dog pics!');
-getRandomPicture()
-  .then((result) => {
-    console.log(result);
-    console.log('3:Done Getting dog pics!');
-  })
-  .catch((err) => {
-    // รับ result ที่เป็น error ใน async...await
-    console.log(err);
-  });
- */
