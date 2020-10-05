08fcbaecc13488de887b4909cd418bef26c4381e
arigatopix <teeruch.k@gmail.com>
2020-10-05 17:18:10 +0700

    85. Creating Documents and Testing the Model

--- section-07-mongoose/server.js
+++ section-07-mongoose/server.js
@@ -13,8 +13,45 @@ mongoose
     // console.log(con.connections);
     console.log('MongoDB Connected...');
   })
-  .catch(error => {
+  .catch(err => {
     throw new Error('MongoDB error');
+  });
+
+// Create simple Tour Model
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour much have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour much have a price']
+  }
+});
+
+// create Model
+const Tour = mongoose.model('Tour', tourSchema);
+
+// create Model instance
+const testTour = new Tour({
+  name: 'The Forest Hill tour',
+  rating: 4.7,
+  price: 497
+});
+
+// Save to MongoDB
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR :', err);
   });
 
 const PORT = config.port || 3000;
