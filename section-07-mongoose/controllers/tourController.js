17f1b1762cbbad179ae8e75c33c410f088feba7c
arigatopix <teeruch.k@gmail.com>
2020-10-06 14:28:35 +0700

    94. Making the API Better: Filtering

--- section-07-mongoose/controllers/tourController.js
+++ section-07-mongoose/controllers/tourController.js
@@ -2,8 +2,17 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD Query
+    const queryObj = { ...req.query };
+    const excludeFileds = ['page', 'sort', 'limit', 'fields'];
+    excludeFileds.forEach(el => delete queryObj[el]);
 
+    const query = Tour.find(queryObj);
+
+    // EXECUTE Query
+    const tours = await query;
+
+    // SEND Response
     res.status(200).json({
       success: true,
       results: tours.length,
