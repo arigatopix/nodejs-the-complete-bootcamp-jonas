589191e57ce1234f4ec38fdc730e33b77a25c38f
arigatopix <teeruch.k@gmail.com>
2020-10-06 15:31:19 +0700

    97. Making the API Better: Limiting Fields

--- section-07-mongoose/controllers/tourController.js
+++ section-07-mongoose/controllers/tourController.js
@@ -26,6 +26,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort('-createdAt');
     }
 
+    // 4) Select
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+
     // EXECUTE Query
     const tours = await query;
 
