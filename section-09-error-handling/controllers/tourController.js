19b4e0b7dd4648b593c518dedea95f95f6e857cf
arigatopix <teeruch.k@gmail.com>
2020-10-09 15:34:19 +0700

    115. Catching Errors in Async Functions

--- section-09-error-handling/controllers/tourController.js
+++ section-09-error-handling/controllers/tourController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const APIFeatures = require('../utils/apiFeatures');
+const catchAsync = require('../utils/catchAsync');
 
 exports.aliasTopTours = (req, res, next) => {
   req.query.limit = '5';
@@ -9,191 +10,141 @@ exports.aliasTopTours = (req, res, next) => {
   next();
 };
 
-exports.getTourStats = async (req, res) => {
-  try {
-    const stats = await Tour.aggregate([
-      {
-        $match: { ratingsAverage: { $gte: 4.5 } }
-      },
-      {
-        $group: {
-          _id: { $toUpper: '$difficulty' },
-          num: { $sum: 1 },
-          numRatings: { $sum: '$ratingQuantity' },
-          avgRatings: { $avg: '$ratingsAverage' },
-          avgPrice: { $avg: '$price' },
-          minPrice: { $min: '$price' },
-          maxPrice: { $max: '$price' }
-        }
-      },
-      {
-        $sort: { avgPrice: 1 }
+exports.getTourStats = catchAsync(async (req, res) => {
+  const stats = await Tour.aggregate([
+    {
+      $match: { ratingsAverage: { $gte: 4.5 } }
+    },
+    {
+      $group: {
+        _id: { $toUpper: '$difficulty' },
+        num: { $sum: 1 },
+        numRatings: { $sum: '$ratingQuantity' },
+        avgRatings: { $avg: '$ratingsAverage' },
+        avgPrice: { $avg: '$price' },
+        minPrice: { $min: '$price' },
+        maxPrice: { $max: '$price' }
       }
-    ]);
+    },
+    {
+      $sort: { avgPrice: 1 }
+    }
+  ]);
 
-    res.status(200).json({
-      success: true,
-      data: {
-        stats
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      data: err
-    });
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: {
+      stats
+    }
+  });
+});
 
-exports.getMonthlyPlan = async (req, res) => {
+exports.getMonthlyPlan = catchAsync(async (req, res) => {
   const year = req.params.year * 1;
-
-  try {
-    const plan = await Tour.aggregate([
-      {
-        $unwind: '$startDates'
-      },
-      {
-        $match: {
-          startDates: {
-            $gte: new Date(`${year}-01-01`),
-            $lte: new Date(`${year}-12-31`)
-          }
+  const plan = await Tour.aggregate([
+    {
+      $unwind: '$startDates'
+    },
+    {
+      $match: {
+        startDates: {
+          $gte: new Date(`${year}-01-01`),
+          $lte: new Date(`${year}-12-31`)
         }
-      },
-      {
-        $group: {
-          _id: { $month: '$startDates' },
-          numTourStarts: { $sum: 1 },
-          tours: { $push: '$name' }
-        }
-      },
-      {
-        $addFields: { month: '$_id' }
-      },
-      {
-        $project: {
-          _id: 0
-        }
-      },
-      {
-        $sort: { numTourStarts: -1 }
       }
-    ]);
-
-    res.status(200).json({
-      success: true,
-      data: {
-        plan
+    },
+    {
+      $group: {
+        _id: { $month: '$startDates' },
+        numTourStarts: { $sum: 1 },
+        tours: { $push: '$name' }
       }
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      data: err
-    });
-  }
-};
-
-exports.getAllTours = async (req, res) => {
-  try {
-    // query คือ cursors ที่ได้จาก mongoDB
-    const features = new APIFeatures(Tour.find(), req.query)
-      .filter()
-      .sort()
-      .limitFields()
-      .paginate();
-
-    // EXECUTE Query
-    const tours = await features.query;
-
-    // SEND Response
-    res.status(200).json({
-      success: true,
-      results: tours.length,
-      data: {
-        tours
+    },
+    {
+      $addFields: { month: '$_id' }
+    },
+    {
+      $project: {
+        _id: 0
       }
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      data: err
-    });
-  }
-};
+    },
+    {
+      $sort: { numTourStarts: -1 }
+    }
+  ]);
 
-exports.getTour = async (req, res) => {
-  try {
-    const tour = await Tour.findById(req.params.id);
-    res.status(200).json({
-      success: true,
-      data: {
-        tour
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      data: err
-    });
-  }
-};
+  res.status(200).json({
+    success: true,
+    data: {
+      plan
+    }
+  });
+});
 
-exports.createTour = async (req, res) => {
-  try {
-    // ใช้ method บน Model
-    const tour = await Tour.create(req.body);
+exports.getAllTours = catchAsync(async (req, res) => {
+  // query คือ cursors ที่ได้จาก mongoDB
+  const features = new APIFeatures(Tour.find(), req.query)
+    .filter()
+    .sort()
+    .limitFields()
+    .paginate();
 
-    res.status(201).json({
-      success: true,
-      data: tour
-    });
-  } catch (err) {
-    res.status(400).json({
-      success: false,
-      message: err
-    });
-  }
-};
+  // EXECUTE Query
+  const tours = await features.query;
 
-exports.updateTour = async (req, res) => {
-  try {
-    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+  // SEND Response
+  res.status(200).json({
+    success: true,
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
 
-    if (!tour) throw new Error('resource not found');
+exports.getTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findById(req.params.id);
+  res.status(200).json({
+    success: true,
+    data: {
+      tour
+    }
+  });
+});
 
-    res.status(200).json({
-      success: true,
-      data: {
-        tour
-      }
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      data: err
-    });
-  }
-};
+exports.createTour = catchAsync(async (req, res) => {
+  // ใช้ method บน Model
+  const tour = await Tour.create(req.body);
 
-exports.deleteTour = async (req, res) => {
-  try {
-    const tour = await Tour.findByIdAndDelete(req.params.id);
+  res.status(201).json({
+    success: true,
+    data: tour
+  });
+});
 
-    if (!tour) throw new Error('resource not found');
+exports.updateTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(404).json({
-      success: false,
-      data: err
-    });
-  }
-};
+  if (!tour) throw new Error('resource not found');
+
+  res.status(200).json({
+    success: true,
+    data: {
+      tour
+    }
+  });
+});
+
+exports.deleteTour = catchAsync(async (req, res) => {
+  const tour = await Tour.findByIdAndDelete(req.params.id);
+
+  if (!tour) throw new Error('resource not found');
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
