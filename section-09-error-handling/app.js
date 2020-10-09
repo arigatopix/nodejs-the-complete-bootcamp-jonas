aa3ec0fd3a7de4f2416541295859c3e78d4bee54
arigatopix <teeruch.k@gmail.com>
2020-10-09 11:29:31 +0700

    113. Implementing a Global Error Handling Middleware

--- section-09-error-handling/app.js
+++ section-09-error-handling/app.js
@@ -22,9 +22,29 @@ app.use('/api/v1/users', userRoutes);
 
 // Handling Unhandle Routes
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server`
+  // });
+
+  // defind error message โดยใช้ built-in Class
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  // send to Global Error Handling Middleware
+  next(err);
+});
+
+// Global Error Handling Middleware
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
   });
 });
 
