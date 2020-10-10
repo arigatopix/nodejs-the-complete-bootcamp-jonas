7b15c91c2927450b34929635be498c26995b46f4
arigatopix <teeruch.k@gmail.com>
2020-10-10 14:38:38 +0700

    121. Errors Outside Express: Unhandled Rejections

--- section-09-error-handling/server.js
+++ section-09-error-handling/server.js
@@ -4,22 +4,29 @@ const config = require('./config');
 
 // connect DB
 (async () => {
-  try {
-    await mongoose.connect(config.mongoURI, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-      useUnifiedTopology: true
-    });
+  await mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  });
 
-    console.log('MongoDB Connected...');
-  } catch (err) {
-    throw new Error('MongoDB error');
-  }
+  console.log('MongoDB Connected...');
 })();
 
 const PORT = config.port || 3000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Sever running on port : ${PORT}`);
+});
+
+// Global unhandledRejection
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLED REJECTION! : Shutting Down...');
+
+  // close server after pending last request
+  server.close(() => {
+    process.exit(1);
+  });
 });
