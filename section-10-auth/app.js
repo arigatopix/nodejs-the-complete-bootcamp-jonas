898de22074f5fe4d212c1fb632aca4a3cc0776a5
arigatopix <teeruch.k@gmail.com>
2020-10-11 19:30:34 +0700

    142. Implementing Rate Limiting

--- section-10-auth/app.js
+++ section-10-auth/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const rateLimit = require('express-rate-limit');
 const morgan = require('morgan');
 const config = require('./config');
 
@@ -20,13 +21,26 @@ if (config.env === 'development') {
   app.use(morgan('dev'));
 }
 
+// Rate limiter 15 นาที / 100 request
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,
+  message:
+    'Too many requests from this IP, please try again in an 15 minutes',
+});
+
+app.use('/api', limiter);
+
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
 // Handling Unhandle Routes
 app.all('*', (req, res, next) => {
   // defind error message โดยใช้ built-in Class
-  const err = new AppError(`Can't find ${req.originalUrl} on this server`, 404);
+  const err = new AppError(
+    `Can't find ${req.originalUrl} on this server`,
+    404,
+  );
 
   // send to Global Error Handling Middleware
   next(err);
