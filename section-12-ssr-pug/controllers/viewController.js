5d670c83bcb61f8b52d9bdfb9d55eaf88a0770be
arigatopix <teeruch.k@gmail.com>
2020-10-18 14:31:31 +0700

    192. Rendering Error Pages

--- section-12-ssr-pug/controllers/viewController.js
+++ section-12-ssr-pug/controllers/viewController.js
@@ -14,11 +14,15 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data, for the requested tour (include reviews and guides)
   const tour = await Tour.findOne({
     slug: req.params.slug,
   }).populate({ path: 'reviews', fields: 'review rating user' });
+
+  if (!tour) {
+    return next(new AppError('There is no tour with that name'));
+  }
 
   // 2) Build template
   // 3) Render that template using your data from 1)
