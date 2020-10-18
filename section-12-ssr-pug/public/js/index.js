632032a00a3e966a8513b4b3e97b35108fc51408
arigatopix <teeruch.k@gmail.com>
2020-10-18 16:44:08 +0700

    195. Updating User Data with Our API

--- section-12-ssr-pug/public/js/index.js
+++ section-12-ssr-pug/public/js/index.js
@@ -1,12 +1,15 @@
 /* eslint-disable */
 import '@babel/polyfill';
 import { login, logout } from './login';
+import { updateData } from './updateSettings';
+
 import { displayMap } from './mapbox';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
 
 // DELEGATION
 if (mapBox) {
@@ -27,3 +30,14 @@ if (loginForm) {
 }
 
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
+
+if (userDataForm) {
+  userDataForm.addEventListener('submit', e => {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    // updateData
+    updateData(name, email);
+
+    e.preventDefault();
+  });
+}
