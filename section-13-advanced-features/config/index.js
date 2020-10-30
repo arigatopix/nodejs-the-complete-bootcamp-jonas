e081275a0ce6470eb33b7a44a3b3dcbcf4ee777b
arigatopix <teeruch.k@gmail.com>
2020-10-30 15:55:00 +0700

    205. Building a Complex Email Handler

--- section-13-advanced-features/config/index.js
+++ section-13-advanced-features/config/index.js
@@ -33,6 +33,7 @@ module.exports = {
     port: process.env.EMAIL_PORT,
     user: process.env.EMAIL_USER,
     password: process.env.EMAIL_PASSWORD,
+    from: process.env.EMAIL_FROM,
   },
   /**
    * API configs */
--- section-13-advanced-features/utils/sendEmail.js
+++ section-13-advanced-features/utils/sendEmail.js
@@ -1,28 +1,60 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 const config = require('../config');
 
-const sendEmail = async options => {
-  // 1) create a transporter object using the default SMTP transport
-  const transporter = nodemailer.createTransport({
-    host: config.email.host,
-    port: config.email.port,
-    auth: {
-      user: config.email.user,
-      pass: config.email.password,
-    },
-  });
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Natours Admin <${config.email.from}>`;
+  }
 
-  // 2) Define the mail options
-  const mailOptions = {
-    from: 'no-reply <no-reply@natours.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html,
-  };
+  newTransport() {
+    if (config.env === 'production') {
+      // Sendgrid
+      return 1;
+    }
 
-  // 3) Actully send the email
-  await transporter.sendMail(mailOptions);
+    // development environment
+    return nodemailer.createTransport({
+      host: config.email.host,
+      port: config.email.port,
+      auth: {
+        user: config.email.user,
+        pass: config.email.password,
+      },
+    });
+  }
+
+  // Send the actual email
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName, // เป็น variable ใช้ใน template
+        url: this.url,
+        subject,
+      },
+    );
+
+    // 2) Define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject: subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  // Send specific preset
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours Family!');
+  }
 };
-
-module.exports = sendEmail;
