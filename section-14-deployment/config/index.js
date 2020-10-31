33ea26edc8f3b4fccc968f2c563f51bd4b1fa1c6
arigatopix <teeruch.k@gmail.com>
2020-10-31 21:49:12 +0700

    load config.env when development mode

--- section-14-deployment/config/index.js
+++ section-14-deployment/config/index.js
@@ -1,12 +1,14 @@
-const dotenv = require('dotenv');
-
-const envFile = dotenv.config({ path: './config/config.env' });
-
 // Set Environment 'development' by default
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (envFile.error) {
-  throw new Error('Could not find .env file');
+if (process.env.NODE_ENV === 'development') {
+  const dotenv = require('dotenv');
+
+  const envFile = dotenv.config({ path: './config/config.env' });
+
+  if (envFile.error) {
+    throw new Error('Could not find .env file');
+  }
 }
 
 module.exports = {
